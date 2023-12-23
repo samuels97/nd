b9bca694bd1a95100f3fdabeee04b281e8f8127a
kalumba Samuel <kalumbasml@gmail.com>
2023-12-23 19:01:35 +0300

    nm

--- xproject/server.js
+++ xproject/server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const data = require('./data.json');
 
 const app = express();
 app.use(express.static('static'));
 
-app.listen(3000, function(){
+app.get('/home', (req, res)=>{
+    const newdata = data.map((datta)=>{
+        const {name, road} = datta
+        return {name}
+    });
+    res.json(newdata);
+});
+
+app.listen(3002, function(){
     console.log('App started on port 3000');
 });
