0c821dd35650cb05663503c1a79dac8ec5e2c214
kalumba Samuel <kalumbasml@gmail.com>
2023-12-27 16:26:33 +0300

    sqlite3

--- temp2/index.js
+++ temp2/index.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-app.use(express.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', function(req, res){
     res.send("Hello world");
 });
 
+app.get('/form', (req, res) => {
+  res.sendFile(__dirname + '/public/form.html');
+});
+
+
 app.post('/submit-form', (req, res) => {
-  const formData = req.body; // Access form data here
+  const formData = req.body;
+  console.log(formData); // Log the form data for testing
+
   // Process the form data and respond accordingly
+  res.send('Form submitted successfully!');
 });
 
 
