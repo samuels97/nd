53939793059ec4c085595801b8f92baea0c868b1
kalumba Samuel <kalumbasml@gmail.com>
2023-12-27 15:23:27 +0300

    form submission

--- temp2/index.js
+++ temp2/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({extended:true}));
+
 app.get('/', function(req, res){
     res.send("Hello world");
 });
+
+app.post('/submit-form', (req, res) => {
+  const formData = req.body; // Access form data here
+  // Process the form data and respond accordingly
+});
+
 
 app.listen(5000);
