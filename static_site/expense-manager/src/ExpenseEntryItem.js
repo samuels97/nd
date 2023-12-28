ed77170e6aba18b2ecda7ffb39cd882e268e05ac
kalumba Samuel <kalumbasml@gmail.com>
2023-12-28 18:11:34 +0300

    more props in ExpenseEntryItem

--- static_site/expense-manager/src/ExpenseEntryItem.js
+++ static_site/expense-manager/src/ExpenseEntryItem.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import './ExpenseEntryItem.css';
+import FormattedMoney from './FormattedMoney';
+import FormattedDate from './FormattedDate';
 
 class ExpenseEntryItem extends React.Component{
     render(){
         return(
             <div className="itemStyle">
-
- <div><b>Item:</b> <em>Mango Juice</em></div>
-
- <div><b>Amount:</b> <em>30.00</em></div>
-
- <div><b>Spend Date:</b> <em>2020-10-10</em></div>
-
- <div><b>Category:</b> <em>Food</em></div>
-
- </div>
+	        <div><b>Item:</b> <em>{this.props.item.name}</em></div>
+		<div><b>Amount:</b> <em>{this.props.item.amount}</em></div>
+		<div><b>Spend Date:</b> <em>{this.props.item.spendDate}</em></div>
+		<div><b>Category:</b> <em>{this.props.item.category}</em></div>
+	    </div>
         );
     }
 }
--- static_site/expense-manager/src/index.js
+++ static_site/expense-manager/src/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ExpenseEntryItem from './ExpenseEntryItem';
 
+const item = {
+	id:1,
+	name:"Grape Juice",
+	amount: 30.5,
+	spendDate: new Date("2010-10-10"),
+	category: "Food"
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <ExpenseEntryItem/>
+    <ExpenseEntryItem item={item}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
