7eb8c5543191a195537cb76d3d2250f273d77766
kalumba Samuel <kalumbasml@gmail.com>
2023-12-28 17:57:52 +0300

    formatted date &money

---
+++ static_site/expense-manager/src/components/FormattedMoney.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class FormattedMoney extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    format(amount){
+        return parseFloat(amount).toFixed(2)
+    }
+
+    render(){
+        <span>{this.format(this.props.value)}</span>
+    };
+}
+
+export default FormattedMoney;
